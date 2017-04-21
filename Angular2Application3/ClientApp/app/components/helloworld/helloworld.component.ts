8daa085f97b1bf11504968a4e8c736799cbd8870
Sean <seon2you@hotmail.com>
2017-04-21 02:05:06 -0700

    first commit and weather api

---
+++ Angular2Application3/ClientApp/app/components/helloworld/helloworld.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'helloworld',
+    template: require('./helloworld.component.html')
+})
+
+export class HelloWorldComponent {
+}
---
+++ Angular2Application3/Controllers/WeatherController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Angular2Application3.Controllers
+{ 
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet("[action]/{city}")]
+        public IActionResult City(string city)
+        {
+            return Ok(new { Temp = "12", Summary = "Barmy", City = "London" });
+        }
+    }
+}
